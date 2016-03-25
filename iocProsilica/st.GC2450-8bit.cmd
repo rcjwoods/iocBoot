2ac74e96bc70d455a43030a3dfa896ba485e2581
Russell Woods <rcjwoods@gmail.com>
2016-03-25 15:28:17 -0500

    minor changes, such as line spacing

--- iocProsilica/st.GC2450-8bit.cmd
+++ iocProsilica/st.GC2450-8bit.cmd
@@ -58,7 +58,7 @@ set_requestfile_path("$(ADPROSILICA)/prosilicaApp/Db")
 # Autosave menu
 dbLoadRecords("$(AUTOSAVE)/asApp/Db/configMenu.db", "P=$(PREFIX),CONFIG=setup")
 set_requestfile_path("$(AUTOSAVE)/asApp/Db")
-save_restoreSet_FilePermissions(0666) 			# Sets autosave files permissions to 0666 or RW for everyone
+save_restoreSet_FilePermissions(0777) 			# Sets autosave files permissions to 0666 or RW for everyone
 set_pass0_restoreFile("auto_settings.sav", "P=$(PREFIX)")
 set_pass1_restoreFile("auto_settings.sav", "P=$(PREFIX)")
 
